36592f3e8565fdb1677ff5c7ca75f0f6840c53a9
Rhys Minchin <rhysminchin@hotmail.co.uk>
2022-12-20 12:30:05 +0000

    finished exercise 2 -inheritance -abstract base classes -interfaces -composition

--- Ch 2/composition_start.py
+++ Ch 2/composition_start.py
@@ -3,23 +3,46 @@
 
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
 
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def add_chapter(self, chapter):
+        self.chapters.append(chapter)
+
+    def get_book_page_count(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.page_count
+        return result
 
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+class Author:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
+
+class Chapter:
+    def __init__(self, name, page_count):
+        self.name = name
+        self.page_count = page_count
+
+
+auth = Author("Leo", "Tolstoy")
+b1 = Book("War and Peace", 39.0, auth)
+
+b1.add_chapter(Chapter("Chapter 1", 125))
+b1.add_chapter(Chapter("Chapter 2", 97))
+b1.add_chapter(Chapter("Chapter 3", 143))
+
+print(b1.author)
 print(b1.title)
+print(b1.get_book_page_count())
--- Ch 2/multiple_start.py
+++ Ch 2/multiple_start.py
@@ -6,17 +6,24 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = "Class B"
 
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def show_props(self):
+        print(self.foo + ", " + self.bar + ", " + self.name)
+
 
 c = C()
+c.show_props()
+print(C.__mro__)
